6463816f75247ec030fe0d2540252aaeb586d0af
jqzhuuuu <joshzhu12@gmail.com>
2021-05-05 15:20:14 -0700

    script.js update

--- scripts/script.js
+++ scripts/script.js
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
    * the current entry for each journal-entry element.
    */
 
-  let url = ""  // SET URL 
+  let url = "https://cse110lab6.herokuapp.com/entries"  // SET URL 
 
   fetch(url)
-    .then( /* FILL IN RESPONSE HANDLING HERE */ )
+    .then(response => response.json())
     .then(entries => {
       entries.forEach((entry) => {
        
@@ -27,8 +27,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         // CODE GOES HERE vvv
-        
 
+        newPost = document.createElement('journal-entry');
+        newPost.entry = entry;
+
+        const main = document.getElementsByTagName('main')[0];
+        main.appendChild(newPost);
+        
 
 
 
